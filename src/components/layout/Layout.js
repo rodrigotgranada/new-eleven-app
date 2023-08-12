bad376aac0611df0b214c0f24c8510c78cf64388
Rodrigo Granada <rodrigotgranada@gmail.com>
2023-08-12 12:26:12 -0300

    nav admin

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,13 +3,14 @@ import Routers from "../../router/Routers";
 import { Nav as PublicNav } from "../public/nav/Nav"
 import { Nav as AdminNav } from "../admin/nav/Nav";
 import { useAuth } from "../../contexts/AuthContext";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
     const { currentUser } = useAuth();
   return (
     <>
-    <AdminNav />
-    <PublicNav />
+    {location.pathname.startsWith("/admin") ? <AdminNav /> : <PublicNav />}
     <Routers />
     </>
   )
